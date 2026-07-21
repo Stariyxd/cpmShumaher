7bce0944401b814b65ccfe3867df0581531e103e
Stariyxd <sg161023@gmail.com>
2026-07-21 15:55:40 +0300

    бд кпап

--- supabase.ts
+++ supabase.ts
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+const supabaseUrl = 'https://vxitfyaodawjeqhzwiue.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ4aXRmeWFvZGF3amVxaHp3aXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2MjkxNjAsImV4cCI6MjEwMDIwNTE2MH0.2zPcxgGYUpMfpNh3E-X10KD1Bls_M1b4cfQr1CPkjBk'
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey)
